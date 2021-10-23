3cf2e7066a27ebe32c27545464bbe40765137e18
GustavoCarvalhopro <gustavor.carvalho@hotmail.com>
2021-10-23 05:16:56 -0300

    first commit

---
+++ Conversão de temperatura.py
@@ -0,0 +1,12 @@
+def main():
+
+   TempF = 78
+   TempC = input ("Entre com o valor de Celsius: ")
+   temp = int (TempC)
+   Celsius = (TempF - temp) * 5 / 9
+
+    print (" A temperatura é : " , Celsius)
+
+    
+main ();
+
